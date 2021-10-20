5b911001b7073abb5448cc627e75531b3efb604b
Sebastian Ruiz Gallego <sebasruigalle62@gmail.com>
2021-10-19 23:44:48 -0500

    Part 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import 'semantic-ui-css/semantic.min.css';
 import { receivePhones } from '../actions/phones';
 import '../index.css';
 import { _getPhones } from '../utils/_DATA';
 import Container from './Container';
-
 
 
 class App extends Component {
